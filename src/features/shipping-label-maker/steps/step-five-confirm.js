e1d4e5457f5d2704a68b0e5275d57973e5b5777a
Danny <ngo.danny11@gmail.com>
2019-06-02 17:04:25 -0700

    final changes with material ui installed

--- src/features/shipping-label-maker/steps/step-five-confirm.js
+++ src/features/shipping-label-maker/steps/step-five-confirm.js
@@ -1,5 +1,10 @@
 import React from "react";
 import { getShippingCost, getShippingOption} from "../../../features/shipping-label-maker/shipping-cost.js";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
 
 const Confirm = (props) => {
     const { from } = props.wizardContext;
@@ -9,24 +14,50 @@ const Confirm = (props) => {
     const { onAction } = props;
     const confirmInfo = () => onAction(props.wizardContext);
     return (
-        <div>
-            <h4>Sender Information</h4>
-            <p> Name: {from.name} </p>
-            <p> Street: {from.street} </p>
-            <p> City: {from.city} </p>
-            <p> State: {from.state} </p>
-            <p> Zip: {from.zip} </p>
-            <h4>Receiver Information</h4>
-            <p> Name: {to.name} </p>
-            <p> Street: {to.street} </p>
-            <p> City: {to.city} </p>
-            <p> State: {to.state} </p>
-            <p> Zip: {to.zip} </p>
-            <p>Weight: {weight.weight} lbs.</p>
-            <p>Shipping option: {getShippingOption(shippingOption.shippingOption)}</p>
-            <p>Shipping cost: ${getShippingCost(weight.weight, shippingOption.shippingOption)}</p>
-            <button onClick={confirmInfo}>Confirm</button>
-        </div>
+        <MuiThemeProvider>
+            <ListSubheader color="primary">Sender Information</ListSubheader>
+            <Typography component="div">
+                <Box fontFamily="Monospace" fontSize="h6.fontSize" m={1}>Name: {from.name} </Box>
+            </Typography>
+            <Typography component="div">
+                <Box fontFamily="Monospace" fontSize="h6.fontSize" m={1}>Street: {from.street} </Box>
+            </Typography>
+            <Typography component="div">
+                <Box fontFamily="Monospace" fontSize="h6.fontSize" m={1}>City: {from.city} </Box>
+            </Typography>
+            <Typography component="div">
+                <Box fontFamily="Monospace" fontSize="h6.fontSize" m={1}>State: {from.state} </Box>
+            </Typography>
+            <Typography component="div">
+                <Box fontFamily="Monospace" fontSize="h6.fontSize" m={1}>Zip Code: {from.zip} </Box>
+            </Typography>
+            <ListSubheader color="primary">Receiver Information</ListSubheader>
+            <Typography component="div">
+                <Box fontFamily="Monospace" fontSize="h6.fontSize" m={1}>Name: {to.name} </Box>
+            </Typography>
+            <Typography component="div">
+                <Box fontFamily="Monospace" fontSize="h6.fontSize" m={1}>Street: {to.street} </Box>
+            </Typography>
+            <Typography component="div">
+                <Box fontFamily="Monospace" fontSize="h6.fontSize" m={1}>City: {to.city} </Box>
+            </Typography>
+            <Typography component="div">
+                <Box fontFamily="Monospace" fontSize="h6.fontSize" m={1}>State: {to.state} </Box>
+            </Typography>
+            <Typography component="div">
+                <Box fontFamily="Monospace" fontSize="h6.fontSize" m={1}>Zip Code: {to.zip} </Box>
+            </Typography>
+            <Typography component="div">
+                <Box fontFamily="Monospace" fontSize="h6.fontSize" m={1}>Weight: {weight.weight} lbs</Box>
+            </Typography>
+            <Typography component="div">
+                <Box fontFamily="Monospace" fontSize="h6.fontSize" m={1}>Shipping Option: {getShippingOption(shippingOption.shippingOption)} </Box>
+            </Typography>
+            <Typography component="div">
+                <Box fontFamily="Monospace" fontSize="h6.fontSize" m={1}>Shipping Cost: ${getShippingCost(weight.weight, shippingOption.shippingOption)} </Box>
+            </Typography>
+            <Button variant="contained" color="primary" onClick={confirmInfo}>Confirm</Button>
+        </MuiThemeProvider>
     )
 };
 
