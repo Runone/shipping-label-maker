18b4ff847e0fb2dc8f7b490363cdb9d986c6b6b8
Danny <ngo.danny11@gmail.com>
2019-06-12 13:42:11 -0700

    added shipping label maker test

--- src/test/shipping-label-maker.test.js
+++ src/test/shipping-label-maker.test.js
@@ -1,5 +1,39 @@
 import React from "react";
-import Enzyme, { shallow, mount } from "enzyme";
+import Enzyme, { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+import ShippingLabelMaker from "../features/shipping-label-maker/shipping-label-maker.js";
+import ShippingLabel from "../features/shipping-label-maker/shipping-label.js";
 
+Enzyme.configure({ adapter: new Adapter() });
 
+describe("ShippingLabelMaker tests", () => {
+    let wrapper;
+    const props = {
+        wizardContext: {
+            from: {
+                name: "name",
+                street: "street",
+                city: "city",
+                state: "state",
+                zip: "zip",
+            },
+            to: {
+                name: "name",
+                street: "street",
+                city: "city",
+                state: "state",
+                zip: "zip",
+            },
+            weight: 11,
+            shippingOption: "1",
+        }
+    };
+
+    describe("ShippingLabelMaker should print a shipping label when createLabel is called", () => {
+        it("should print a shipping label when createLabel is called", () => {
+            wrapper = shallow(<ShippingLabelMaker {...props}/>);
+            wrapper.instance().createLabel(props.wizardContext);
+            expect(wrapper.find(ShippingLabel).length).toEqual(1);
+        });
+    });
+});
